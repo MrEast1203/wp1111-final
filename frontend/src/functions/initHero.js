bdc54e73b1e0338bb44cc529fed9e10e2e9415ee
Kan <mreast1203@gmail.com>
2023-01-04 21:54:17 +0800

    test

--- frontend/src/functions/initHero.js
+++ frontend/src/functions/initHero.js
@@ -15,7 +15,19 @@ const initHero = async (
 ) => {
   console.log("id", id);
   const data = await getHero(id)
-    .then((data) => console.log("data", data))
+    .then((data) => {
+      console.log("data", data);
+      setPlayerName(data[0].name);
+      setEnergy(data[0].battle_card);
+      setAtk(data[0].atk);
+      setHp(data[0].life);
+      setMax_hp(data[0].max_life);
+      setMoney(data[0].money);
+      setItemsForDB(data[0].item);
+      setAchieveForDB(data[0].achieve);
+      setDay(data[0].turn);
+      setDeathRate(data[0].deathRate);
+    })
     .catch((err) => console.log("err", err));
   // console.log("id", id);
   // console.log("loginstate", loginState);
@@ -24,16 +36,16 @@ const initHero = async (
   //   // console.log("here");
   // } else data = await getHero("-1fsafgsg");
   console.log("hi", data);
-  setPlayerName(data[0].name);
-  setEnergy(data[0].battle_card);
-  setAtk(data[0].atk);
-  setHp(data[0].life);
-  setMax_hp(data[0].max_life);
-  setMoney(data[0].money);
-  setItemsForDB(data[0].item);
-  setAchieveForDB(data[0].achieve);
-  setDay(data[0].turn);
-  setDeathRate(data[0].deathRate);
+  // setPlayerName(data[0].name);
+  // setEnergy(data[0].battle_card);
+  // setAtk(data[0].atk);
+  // setHp(data[0].life);
+  // setMax_hp(data[0].max_life);
+  // setMoney(data[0].money);
+  // setItemsForDB(data[0].item);
+  // setAchieveForDB(data[0].achieve);
+  // setDay(data[0].turn);
+  // setDeathRate(data[0].deathRate);
   return data;
 };
 
