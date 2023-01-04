5ed24b6f825c90ddd758adc7fe86fcc92df8b0f7
Kan <mreast1203@gmail.com>
2023-01-04 19:53:21 +0800

    debug inithero_v2

--- backend/src/routes/hero.js
+++ backend/src/routes/hero.js
@@ -1,10 +1,10 @@
-import Hero from '../models/hero';
+import Hero from "../models/hero";
 
 exports.GetHero = async (req, res) => {
   const id = req.query.id;
   await Hero.find({ id: id }).exec((err, data) => {
-    if (!err) res.status(200).send({ message: 'success', contents: data });
-    else res.status(403).send({ message: 'error', contents: [] });
+    if (!err) res.status(200).send({ message: "success", contents: data });
+    else res.status(403).send({ message: "error", contents: [] });
   });
 };
 
@@ -13,10 +13,10 @@ exports.CreateHero = async (req, res) => {
   const hero = new Hero(body);
   try {
     await hero.save();
-    res.status(200).send({ message: 'success', contents: body });
+    res.status(200).send({ message: "success", contents: body });
   } catch (e) {
     console.log(err);
-    res.status(403).send({ message: 'error', contents: [] });
+    res.status(403).send({ message: "error", contents: [] });
   }
 };
 //ffff
@@ -24,9 +24,9 @@ exports.UpdateHero = async (req, res) => {
   const body = req.body;
   try {
     updated_hero = await Hero.updateOne({ id: body.id }, { $set: body });
-    res.status(200).send({ message: 'success', contents: updated_hero });
-  } catch (e) {
+    res.status(200).send({ message: "success", contents: updated_hero });
+  } catch (err) {
     console.log(err);
-    res.status(403).send({ message: 'error', contents: [] });
+    res.status(403).send({ message: "error", contents: [] });
   }
 };
